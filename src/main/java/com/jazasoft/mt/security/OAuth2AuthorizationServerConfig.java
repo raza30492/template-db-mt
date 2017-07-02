c04b1c48e9919886913cd86ad9535f722ec7af24
Md Zahid Raza <zahid7292@ymail.com>
2017-07-02 17:59:02 +0530

    added Yaml Utility Class

--- src/main/java/com/jazasoft/mt/security/OAuth2AuthorizationServerConfig.java
+++ src/main/java/com/jazasoft/mt/security/OAuth2AuthorizationServerConfig.java
@@ -1,5 +1,6 @@
 package com.jazasoft.mt.security;
 
+import com.jazasoft.mt.util.YamlUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.context.EnvironmentAware;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.builders.InMemoryClientDetailsServiceBuilder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -17,14 +19,14 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @EnableAuthorizationServer
-public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter implements EnvironmentAware{
+public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
     private static String REALM="MY_OAUTH_REALM";
     private static final String ENV_OAUTH2 = "security.oauth2.client.";
-    
-    private RelaxedPropertyResolver propertyResolver;
  
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -33,15 +35,21 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
  
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
- 
-        clients.inMemory()
-            .withClient(propertyResolver.getProperty("client1.id"))
-            .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
-            .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
-            .scopes(propertyResolver.getProperty("client1.scope"))
-            .secret(propertyResolver.getProperty("client1.secret"))
-            .accessTokenValiditySeconds(propertyResolver.getProperty("client1.access-token-validity", Integer.class, 300))
-            .refreshTokenValiditySeconds(propertyResolver.getProperty("client1.refresh-token-validity", Integer.class, 600));
+
+        List clientList = (List) YamlUtils.getINSTANCE().getAppProperty("security.oauth2.clients");
+
+        InMemoryClientDetailsServiceBuilder builder = clients.inMemory();
+        for (int i = 0; i < clientList.size(); i++) {
+            Map client = (Map) clientList.get(i);
+            builder
+                    .withClient((String) client.get("id"))
+                    .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+                    .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+                    .scopes((String) client.get("scope"))
+                    .secret((String) client.get("secret"))
+                    .accessTokenValiditySeconds(Integer.parseInt((String) (String) client.get("access-token-validity")))
+                    .refreshTokenValiditySeconds(Integer.parseInt((String) (String) client.get("refresh-token-validity")));
+        }
     }
  
     @Override
@@ -53,11 +61,6 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
     @Override
     public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
         oauthServer.realm(REALM+"/client");
-    }
-
-    @Override
-    public void setEnvironment(Environment environment) {
-        this.propertyResolver = new RelaxedPropertyResolver(environment, ENV_OAUTH2);
     }
 
     @Bean
---
+++ src/test/java/com/jazasoft/mt/util/YamlUtilsTest.java
@@ -0,0 +1,53 @@
+package com.jazasoft.mt.util;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by mdzahidraza on 02/07/17.
+ */
+public class YamlUtilsTest {
+
+    private YamlUtils yamlUtils;
+    File testFile;
+
+    @Before
+    public void setUp() {
+        yamlUtils = YamlUtils.getINSTANCE();
+        String filename = Utils.getAppHome() + File.separator + "conf" + File.separator + "test.yaml";
+        testFile = new File(filename);
+    }
+
+    @Test
+    public void testGetFileProprty() throws IOException{
+        Assert.assertEquals("Md Zahid Raza",yamlUtils.getProperty(testFile, "name"));
+        Assert.assertEquals("25",yamlUtils.getProperty(testFile, "age"));
+        Assert.assertEquals("Bangalore",yamlUtils.getProperty(testFile, "address.city"));
+        Assert.assertEquals("India",yamlUtils.getProperty(testFile, "address.country"));
+
+        List list = (List)yamlUtils.getProperty(testFile, "phones");
+        Assert.assertEquals(2, list.size());
+        Assert.assertEquals("8987525008", ((Map)list.get(0)).get("number"));
+        Assert.assertEquals("8904360418", ((Map)list.get(1)).get("number"));
+    }
+
+    @Test
+    public void testAppProperty() throws IOException{
+        Assert.assertEquals("8085",yamlUtils.getAppProperty("server.port"));
+    }
+
+    @Test
+    public void testNestedProperty() throws IOException{
+        Map schema = (Map)yamlUtils.getProperty(testFile,"schema");
+        Assert.assertEquals("schema-mysql.sql", yamlUtils.getNestedProperty(schema,"mysql.init.filename"));
+        Assert.assertEquals("schema-pg.sql", yamlUtils.getNestedProperty(schema,"postgresql.init.filename"));
+    }
+
+}
